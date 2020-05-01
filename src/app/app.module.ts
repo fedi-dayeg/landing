d098597da69b87dd3c6898e2395d5a0ab33d800e
fedi <fedi.dayeg@hotmail.com>
2020-05-01 01:21:51 +0100

    app not yet finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {WeatherModule} from './weather/weather.module';
+import {HttpClientModule} from '@angular/common/http';
+import {NotificationsModule} from './notifications/notifications.module';
+import {NewsApiModule} from './news-api/news-api.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WeatherModule,
+    HttpClientModule,
+    NotificationsModule,
+    NewsApiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
